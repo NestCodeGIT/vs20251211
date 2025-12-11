8c58fcaab0cfd790ea360887d6a8b7c679712258
Khabat-M <101479708+NestCodeGIT@users.noreply.github.com>
2025-12-11 11:10:46 +0100

    edit Mittelwert

--- FirstProject/Aufgaben/Mittelwert.cs
+++ FirstProject/Aufgaben/Mittelwert.cs
@@ -1,38 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq; 
+
 
 namespace FirstProject.Aufgaben
 {
     public class Mittelwert
     {
-
-
         private int[] zahlen;
         private Random zufall;
 
+       
         public Mittelwert(int größe)
         {
+           
+            if (größe < 3)
+            {
+                größe = 3;
+                Console.WriteLine("Hinweis: Array-Größe auf Minimum 3 gesetzt.");
+            }
+
             zahlen = new int[größe];
             zufall = new Random();
 
+           
             for (int i = 0; i < zahlen.Length; i++)
             {
                 zahlen[i] = zufall.Next(1, 100); 
-             
+           
             }
         }
 
         public void Berechnen()
         {
-            Console.WriteLine("Zahlen: " + string.Join(", ", zahlen));
+           
+            Console.WriteLine(string.Join(", ", zahlen));
 
+          
             int min = zahlen.Min();
             int max = zahlen.Max();
 
-            int summe = zahlen.Sum() - min - max;
-            double mittelwert = summe / (double)(zahlen.Length - 2);
+            int gesamtSumme = zahlen.Sum();
+            int summeOhneExtrem = gesamtSumme - min - max;
 
+          
+            double mittelwert = summeOhneExtrem / (double)(zahlen.Length - 2);
+
+        
             Console.WriteLine($"Mittelwert ohne Min und Max: {mittelwert:F3}");
         }
     }
