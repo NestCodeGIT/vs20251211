72a83c12b9c2f1aac62d429be08696a4f75c7901
Khabat-M <101479708+NestCodeGIT@users.noreply.github.com>
2025-12-12 11:57:18 +0100

    edit

---
+++ Bank-Account/Banl-account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Account
+{
+    internal class Banl_account
+        //felder 
+
+    {
+        private decimal _balance;
+        public string? Owner;
+
+        //methoden
+
+        public decimal getBalance()
+        {
+            return _balance;
+        }
+
+
+    }
+}
---
+++ Bank-Account/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Bank_Account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banl_account account = new Banl_account();
+            //Banl_account account = new ();
+            account.Owner = "John Doe";
+
+                decimal bal =account.getBalance();
+            Console.WriteLine(bal);
+
+            Banl_account account2 = new Banl_account();
+            account2.Owner = "Johne doe";
+
+
+
+        }
+    }
+}
